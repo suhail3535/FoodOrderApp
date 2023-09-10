c1cb3692f18fe1351e60f2315d2510e1152d298f
suhail3535 <khansohail015@gmail.com>
2023-09-10 09:15:32 +0530

    added

--- foodorderapp/src/Pages/OrderPlaced.jsx
+++ foodorderapp/src/Pages/OrderPlaced.jsx
@@ -18,7 +18,7 @@ const OrderPlaced = () => {
     return (
         <div>
             <Navbar />
-            <div style={{ width: "50%", margin: "800px auto" }}>
+            <div style={{ width: "70%", margin: "auto", marginTop: "10rem" }}>
                 <img
                     src="https://cdn.dribbble.com/users/2572904/screenshots/17169793/media/ed801ffe0fbeb4b95ca246ba1f5ea398.gif"
                     alt=""
